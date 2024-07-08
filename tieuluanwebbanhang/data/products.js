bd43c6434f948738a634c3cb1f333f6aff1e892f
TuanAnhh86-dev <vuongcongtuan2005@gmail.com>
2024-07-08 09:24:23 +0700

    Add files via upload

---
+++ tieuluanwebbanhang/data/products.js
@@ -0,0 +1,394 @@
+var list_products = [{
+    "name": "Kiwi vàng Newzealand (2Trái/Hộp)",
+    "img": "https://hcm.fstorage.vn/images/2022/hộp)_914d2a7c-c9e7-4d17-8271-d9bc8810323d-og-thumb-1.png",
+    "price": "69.000  ",
+    "star": 4,
+    "rateCount": 26,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+
+}, {
+    "name": "Táo Envy NewZealand 90 - 110",
+    
+    "img": "https://hcm.fstorage.vn/images/2023/07/tao-20230721075400.png",
+    "price": "169.000 .",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    
+  
+}, {
+    "name": "Nước lau sàn hương lily Gift chai 1L",
+ 
+    "img": "https://hcm.fstorage.vn/images/2023/09/10019677-20230926065607.png",
+    "price": "150.000",
+    "star": 5,
+    "rateCount": 188,
+    "promo": {
+        "name": "giamgia",
+        "value": "20.000"
+    },
+    
+
+}, {
+    "name": "Nước giặt Ariel Hương oải hương túi 2kg/1.8kg",
+    
+    "img": "https://hcm.fstorage.vn/images/2022/1_8f77a39c-7d91-4f96-abcb-f33a6697c152-og.8kg",
+    "price": "120.000",
+    "star": 5,
+    "rateCount": 7,
+    "promo": {
+        "name": "giamgia",
+        "value": "20.000"
+    },
+   
+}, {
+    "name": "Nước giặt xả đậm đặc tím Carefor can 3.5L",
+    "company": "Nước lau sàn",
+    "img": "https://hcm.fstorage.vn/images/2022/162428526644610019476-can-to-quan-nang-hoat-caryn-l-tyi-14-mieng-og.jpg",
+    "price": "130.000",
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "10.000"
+    },
+    "masp": "Nước lau sàn"
+}, {
+    "name": "Tôm thẻ nõn HDC hộp 200g",
+    
+    "img": "https://hcm.fstorage.vn/images/2024/04/10282371-hdc-tom-the-non-tu-nhien-200g-20240404082107.jpg",
+    "price": "180.000",
+    "star": 4,
+    "rateCount": 10,
+    "promo": {
+        "name": "giareonline",
+        "value": "230.000"
+    },
+    
+
+
+}, {
+    "name": "Nước giặt xả cho bé Carefor Plus hương hoa hồng chai 3.5 lít",
+    "company": "Samsung",
+    "img": "https://hcm.fstorage.vn/images/2022/10019475.jpg",
+    "price": "150.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "20.000"
+    },
+    
+}, {
+    "name": "Lá Kim Miwon tẩm dầu Ô Liu 15g",
+   
+    "img": "https://hcm.fstorage.vn/images/2023/09/10181043_optimized-20230920092904.png",
+    "price": "90.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giareonline",
+        "value": "110.000"
+    },
+    
+}, {
+    "name": "Dầu gội dưỡng tóc bóng mượt Tsubaki chai 490ml",
+
+    "img": "https://hcm.fstorage.vn/images/2022/tsubaki-dau-goi-duong-toc-bongmuot-490ml_ac63a033-b8ad-48f3-af9c-29eb167e9f48-og.jpg",
+    "price": "450.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "",
+        "value": "0"
+    },
+    
+
+  
+}, {
+    "name": "Lốc 4 hộp sữa hạt và gạo lứt đỏ TH true NUT 180ml",
+  
+    "img": "https://hcm.fstorage.vn/images/2022/loc-4-hop-sua-hat-va-gao-lut-do-th-true-nut-180ml_166f6a38-2f72-4f00-84e0-7967f262238d-og.png",
+    "price": "23.000 .",
+    "star": 4,
+    "rateCount": 104,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    
+    "masp": "sữa"
+}, {
+    "name": "Mì Chapaghetti Nongshim gói 140g",
+
+    "img": "https://hcm.fstorage.vn/images/2022/162427603697610008053-g1-mo-soon-veggie-ramyun-nongshim-gui-112g-og.jpg",
+    "price": "140.000",
+    "star": 4,
+    "rateCount": 80,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    
+}, {
+    "name": "Sữa chua lên men tự nhiên Yomost cam hộp 1L",
+
+    "img": "https://hcm.fstorage.vn/images/2022/yomost-sua-chua-len-men-tn-cam-1l-12_0d348abb-351c-4f31-a41c-1cb5af488b68-og.jpg",
+    "price": "33.000 .",
+    "star": 5,
+    "rateCount": 87,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    
+    "masp": "sữa"
+}, {
+    "name": "Sữa chua uống lên men Yogu Nestle có chứa yến lốc 4 hộp x 115ml",
+ 
+    "img": "https://hcm.fstorage.vn/images/2022/162428159903210623218-g4-sua-nuoc-nestle-ot-duong-loc-4-hop-x-180ml-og.jpg",
+    "price": "24.000 .",
+    "star": 4,
+    "rateCount": 372,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "masp": "sữa"
+
+}, {
+    "name": "GUMI Mì hương vị bún Bò 75gr T30",
+
+    "img": "https://hcm.fstorage.vn/images/2024/02/gumi-t-20240228064157.png",
+    "price": "210.000",
+    "star": 4,
+    "rateCount": 347,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+  
+}, {
+    "name": "Lê Nam Phi",
+    
+    "img": "https://hcm.fstorage.vn/images/2022/le-nam-phi_7ae2df26-6191-4ef0-ae1e-f76a6b80487a-og.jpg",
+    "price": "87.000 .",
+    "star": 4,
+    "rateCount": 22,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    
+ 
+}, {
+    "name": "Phô mai lát Sandwich Zott gói 200g",
+
+    "img": "https://hcm.fstorage.vn/images/2022/162427166825810005842-g1-phu-mai-lot-sandwich-zott-gui-200g-og.jpg",
+    "price": "54.000",
+    "star": 5,
+    "rateCount": 54,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+   
+
+}, {
+    "name": "Dừa tiện lợi",
+    
+    "img": "https://hcm.fstorage.vn/images/2023/07/dua-20230721090949.jpg",
+    "price": "50.000 .",
+    "star": 4,
+    "rateCount": 22,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    
+}, {
+    "name": "Sữa chua Vinamilk nha đam ít đường hộp 100g",
+    "img": "https://hcm.fstorage.vn/images/2023/08/10197309-20230814104024.png",
+    "price": "72.000",
+    "star": 5,
+    "rateCount": 16,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    
+    "masp": "sữa"
+}, {
+    "name": "Sữa hạt óc chó TH True NUT lốc 4 hộp x 180ml",
+
+    "img": "https://hcm.fstorage.vn/images/2022/th-milk-sua-hat-oc-cho-180ml-4_997584f8-eeb1-4f34-995c-a389f9dab336-og.jpg",
+    "price": "49.000",
+    "star": 5,
+    "rateCount": 7,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    
+    "masp": "sữa"
+
+
+}, {
+    "name": "Hạt nêm Chin-Su gói 1Kg",
+
+    "img": "https://hcm.fstorage.vn/images/2023/04/chin-su-hat-nem-thom-ngot-thit-1kg-t8-20230420045948.png",
+    "price": "45.000",
+    "star": 3,
+    "rateCount": 30,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    
+
+
+}, {
+"name": "Táo Xanh",
+    "company": "Trái cây",
+    "img": "https://hcm.fstorage.vn/images/2023/02/fru00115-5-20230227040709.jpg",
+    "price": "34.000. ",
+    "star": 3,
+    "rateCount": 26,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.0', HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "13 MP",
+        "camaraFront": "5 MP",
+        "cpu": "Qualcomm Snapdragon 425 4 nhân 64-bit",
+        "ram": "2 GB",
+        "rom": "16 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "3300 mAh"
+    },
+    "masp": "Sam0"
+}, {
+    "name": "Trứng gà ta quê 729 Ba Vì giỏ 10 quả",
+  
+    "img": "https://hcm.fstorage.vn/images/2022/trung-ga-ta-que-729-ba-vi-gio-10-qua.jpg",
+    "price": "55.555",
+    "star": 4,
+    "rateCount": 37,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+   
+}, {
+    "name": "Xúc xích lắc Heo Cao Bồi thịt nướng phô mai 5*15g",
+    
+    "img": "https://hcm.fstorage.vn/images/2023/02/xuc-xich-lac-heo-cao-boi-thit-nuong-pho-mai-5-15g.png",
+    "price": "9.000",
+    "star": 4,
+    "rateCount": 58,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    
+    
+
+}, {
+    "name": "Vỉ 2 bút Thiên Long GEL08",
+
+    "img": "https://hcm.fstorage.vn/images/2022/vi-2-but-thien-long-gel08-20221021095104-og.jpg",
+    "price": "16.000",
+    "star": 5,
+    "rateCount": 302,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+ 
+}, {
+    "name": "Sữa hạt Macca TH True NUT thùng 48 hộp x 180ml",
+   
+    "img": "https://hcm.fstorage.vn/images/2022/sua-hat-macca-th-true-nut-loc-4-hop-180ml_c2323699-720c-40dd-901c-da47a86d356f-og.jpg",
+    "price": "170.000",
+    "star": 5,
+    "rateCount": 33,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    
+    "masp": "sữa"
+
+
+}, {
+    "name": "Sữa chua ăn Vinamilk lựu đỏ Ít đường 100g",
+    
+    "img": "https://hcm.fstorage.vn/images/2023/05/sua-chu-an-vinamilk-luu-do-it-duong-100g-20230524025651.jpg",
+    "price": "50.000 .",
+    "star": 4,
+    "rateCount": 12,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    
+    "masp": "sữa"
+
+}, {
+    "name": "Cá viên cốm non Hoa Doanh 500g",
+   
+    "img": "https://hcm.fstorage.vn/images/2022/9ac87bc793a80d1d69c0b94909b7853d.jpg",
+    "price": "70.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giareonline",
+        "value": "80.000"
+    },
+   
+}, {
+    "name": "Dưa lê kim hoàng hậu",
+    "img": "https://hcm.fstorage.vn/images/2022/dua-le-kim-hoang-hau-wineco-thumb-1.jpg",
+    "price": "70.000 .",
+    "star": 5,
+    "rateCount": 230,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    
+    "masp": "App4"
+
+}, {
+    "name": "Dầu ăn Tường An chai 1 lít",
+
+    "img": "https://hcm.fstorage.vn/images/2023/07/dau-an-20230707061620.png",
+    "price": "46.000",
+    "star": 4,
+    "rateCount": 5,
+    "promo": {
+        "name": "giareonline",
+        "value": "60.000"
+    },
+
+}, {
+    "name": "Cua đồng xay",
+
+    "img": "https://hcm.fstorage.vn/images/2022/cua-dong-xay_5c2d3388-7378-41b9-ba49-bdc6b4684ce2-og.jpg",
+    "price": "110.000",
+    "star": 4,
+    "rateCount": 804,
+    "promo": {
+        "name": "giareonline",
+        "value": "130.000"
+    },
+    
+}]
